3afeb92e837b1cd00fe46e31564a0c334c6ab6a8
Rajvansh <106300079+steve-369@users.noreply.github.com>
2023-08-05 13:44:58 +0530

    style livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link href="css/responsive.css" rel="stylesheet">
     <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
-    @livewireStyles
+    @livewireStyles()
     <script src="js/jquery.js"></script>
 </head>
 
@@ -82,7 +82,7 @@
     <script src="js/wow.js"></script>
     <script src="js/parallax.min.js"></script>
     <script src="js/custom-script.js"></script>
-    @livewireScripts
+    @livewireScripts()
 
 
 </body>
